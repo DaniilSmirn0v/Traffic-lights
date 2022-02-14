8870ad9df878f261991a4184e72e74e278057794
DaniilSmirn0v <daniil.smirnov.05@mail.ru>
2022-02-14 17:09:33 +0300

    Initial Commit

---
+++ Traffic lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic lights
+//
+//  Created by Даниил Смирнов on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
